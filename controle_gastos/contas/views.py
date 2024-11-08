6fc9a39540f11964daa69bf9b8b053736f21e692
LucasHL7 <lhls2002@gmail.com>
2024-11-08 08:44:02 -0300

    att 0.7

--- controle_gastos/contas/views.py
+++ controle_gastos/contas/views.py
@@ -28,3 +28,23 @@ def nova_transacao(request):
         return redirect('url_listagem')
 
     return render(request, "contas/form.html", data)
+
+def update(request, pk):
+    data = {}
+    transacao = Transacao.objects.get(pk=pk)
+    form = TransacaoForm(request.POST or None, instance=transacao)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+    data['form'] = form
+    data['transacao'] = transacao
+    return render(request, 'contas/form.html', data)
+
+def delete(request, pk):
+    transacao = Transacao.objects.get(pk=pk)
+    transacao.delete()
+    return redirect ('url_listagem')
+
+
+    
